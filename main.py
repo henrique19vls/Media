1a11c02511baa02bd1c904a7a9999d40cf0c1e9c
Henrique Bortolo <henrique.bortolo2007@gmail.com>
2024-05-18 12:49:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+n1 = float(input("Digite a nota 01:"))
+n2 = float(input("Digite a nota 02:"))
+n3 = float(input("Digite a nota 03:"))
+n4 = float(input("Digite a nota 04:"))
+
+média = (n1+n2+n3+n4) / 4
+
+print (f"A media é de {média}")
